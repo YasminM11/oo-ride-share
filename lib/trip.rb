5198feb0ca1122c3dfbdacb665c671bd961df59b
Yasmin <yasmin312002@hotmail.com>
2019-08-26 16:21:13 -0700

    added wave 1.1 all tests are passed

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,37 +1,46 @@
 require 'csv'
+require 'time'
 
 require_relative 'csv_record'
 
 module RideShare
   class Trip < CsvRecord
-    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
-
+    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating, :total
+    
     def initialize(id:,
       passenger: nil, passenger_id: nil,
       start_time:, end_time:, cost: nil, rating:)
       super(id)
-
+      
       if passenger
         @passenger = passenger
         @passenger_id = passenger.id
-
+        
       elsif passenger_id
         @passenger_id = passenger_id
-
+        
       else
         raise ArgumentError, 'Passenger or passenger_id is required'
       end
-
+      
       @start_time = start_time
       @end_time = end_time
       @cost = cost
       @rating = rating
-
+      
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
     end
-
+    
+    
+    def duration
+      if @end_time < @start_time
+        raise ArgumentError.new("Invalid Time - End Time is before Start Time")
+      end
+      return @end_time - @start_time
+    end
+    
     def inspect
       # Prevent infinite loop when puts-ing a Trip
       # trip contains a passenger contains a trip contains a passenger...
@@ -39,23 +48,23 @@ module RideShare
       "ID=#{id.inspect} " +
       "PassengerID=#{passenger&.id.inspect}>"
     end
-
+    
     def connect(passenger)
       @passenger = passenger
       passenger.add_trip(self)
     end
-
+    
     private
     
     def self.from_csv(record)
       return self.new(
         id: record[:id],
         passenger_id: record[:passenger_id],
-        start_time: record[:start_time],
-        end_time: record[:end_time],
+        start_time: Time.parse(record[:start_time]),
+        end_time: Time.parse(record[:end_time]),
         cost: record[:cost],
         rating: record[:rating]
-        )
+      )
     end
   end
 end
--- test/trip_test.rb
+++ test/trip_test.rb
@@ -8,36 +8,59 @@ describe "Trip class" do
       @trip_data = {
         id: 8,
         passenger: RideShare::Passenger.new(id: 1,
-                                            name: "Ada",
-                                            phone_number: "412-432-7640"),
-        start_time: start_time,
-        end_time: end_time,
-        cost: 23.45,
-        rating: 3
-      }
-      @trip = RideShare::Trip.new(@trip_data)
-    end
-
-    it "is an instance of Trip" do
-      expect(@trip).must_be_kind_of RideShare::Trip
-    end
-
-    it "stores an instance of passenger" do
-      expect(@trip.passenger).must_be_kind_of RideShare::Passenger
-    end
-
-    it "stores an instance of driver" do
-      skip # Unskip after wave 2
-      expect(@trip.driver).must_be_kind_of RideShare::Driver
-    end
-
-    it "raises an error for an invalid rating" do
-      [-3, 0, 6].each do |rating|
-        @trip_data[:rating] = rating
-        expect do
-          RideShare::Trip.new(@trip_data)
-        end.must_raise ArgumentError
+          name: "Ada",
+          phone_number: "412-432-7640"),
+          start_time: start_time,
+          end_time: end_time,
+          cost: 23.45,
+          rating: 3
+        }
+        @trip = RideShare::Trip.new(@trip_data)
+      end
+      
+      it "calculates duration" do
+        expect(@trip.duration).must_equal (25 * 60)
+      end
+      
+      it "raises argument error if end date is after start date" do
+        
+        @trip_data = {
+          id: 8,
+          passenger: RideShare::Passenger.new(id: 1,
+            name: "Ada",
+            phone_number: "412-432-7640"),
+            start_time: Time.parse('2015-05-20T12:14:00+00:00') + 25 * 60,
+            end_time: Time.parse('2015-05-20T12:14:00+00:00'),
+            cost: 23.45,
+            rating: 3
+          }
+          
+          @trip = RideShare::Trip.new(@trip_data)
+          expect do @trip.duration end.must_raise ArgumentError 
+          end
+        
+        it "is an instance of Trip" do
+          expect(@trip).must_be_kind_of RideShare::Trip
+        end
+        
+        it "stores an instance of passenger" do
+          expect(@trip.passenger).must_be_kind_of RideShare::Passenger
+        end
+        
+        it "stores an instance of driver" do
+          skip # Unskip after wave 2
+          expect(@trip.driver).must_be_kind_of RideShare::Driver
+        end
+        
+        it "raises an error for an invalid rating" do
+          [-3, 0, 6].each do |rating|
+            @trip_data[:rating] = rating
+            expect do
+              RideShare::Trip.new(@trip_data)
+            end.must_raise ArgumentError
+          end
+        end
       end
     end
-  end
-end
+
+    
